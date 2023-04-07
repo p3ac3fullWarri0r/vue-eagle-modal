9b61eada9c9183ac1901b15f1980c54766764b5d
p3ac3fullWarri0r <timmyb12@gmx.de>
2023-04-07 09:31:12 +0200

    style: re-format some files

--- src/components/types/input.vue
+++ src/components/types/input.vue
@@ -1,16 +1,15 @@
 <template>
-	<div class="item-row">
-		<label v-if='label' class="col label" :for='id'>{{ label }}</label>
-		<div class="col-data">
-			<input :type="type" v-model='val' class="form-control" :id='id' v-bind="attr"/>
-		</div>
-	</div>
+    <div class="item-row">
+        <label v-if="label" class="col label" :for="id">{{ label }}</label>
+        <div class="col-data">
+            <input :type="type" v-model="val" class="form-control" :id="id" v-bind="attr" />
+        </div>
+    </div>
 </template>
 <script>
-  	import Parent from './Parent';
-	export default {
-		extends: Parent,
-		mounted(){
-		}
-	}
+import Parent from './Parent';
+export default {
+    extends: Parent,
+    mounted() {},
+};
 </script>
